77d70132936025a42d4e64b57151dfee21bacbd4
Ejiroghene Philip Wilkie <48595932+wilkieE@users.noreply.github.com>
2022-08-17 09:57:30 +0300

    First commit

---
+++ components/Layout.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Head from 'next/head';
+import Navbar from './Navbar';
+import Foooter from './Footer';
+
+const Layout = ({ children }) => {
+  return (
+    <div className='layout'>
+      <Head>
+        <title>Ejiroghene's First Store</title>
+      </Head>
+      <header>
+        <Navbar/>
+      </header>
+      <main className='main-container'>
+        {children}
+      </main>
+      <footer>
+        <Foooter/>
+      </footer>
+
+    </div>
+  )
+}
+
+export default Layout
