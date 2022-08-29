1d948d34b00ea720f9b0643650547cb08b3a1ad0
Ejiroghene Philip Wilkie <48595932+wilkieE@users.noreply.github.com>
2022-08-29 11:03:59 +0300

    Completed Cart Functionality

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -27,6 +27,7 @@ const ProductDetails = ({ product, products }) => {
           <div className="small-image-container">
             {image?.map((item, i) => (
               <img
+                key={i}
                 src={urlFor(item)}
                 className={
                   i === index ? "small-image selected-image" : "small-image"
